6bb060996c06c2b650b894dd0b003ff34fa45838
Ivan5 <converse_ivan@hotmail.com>
2020-03-24 12:41:57 -0600

    Make a ListTodos component

---
+++ client/src/components/ListTodos.jsx
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from "react";
+
+export default function ListTodos() {
+  const [todos, setTodos] = useState([]);
+
+  //delet todo
+  async function deleteTodo(id) {
+    try {
+      const res = await fetch(`http://localhost:5000/todos/${id}`, {
+        method: "DELETE"
+      });
+
+      setTodos(todos.filter(todo => todo.todo_id !== id));
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+
+  async function getTodos() {
+    const res = await fetch("http://localhost:5000/todos");
+
+    const todoArray = await res.json();
+
+    setTodos(todoArray);
+  }
+
+  useEffect(() => {
+    getTodos();
+  }, []);
+
+  return (
+    <>
+      {" "}
+      <table className="table mt-5">
+        <thead>
+          <tr>
+            <th>Description</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {todos.map(todo => (
+            <tr key={todo.todo_id}>
+              <td>{todo.description}</td>
+              <td>Edit</td>
+              <td>
+                <button
+                  className="btn btn-danger"
+                  onClick={() => deleteTodo(todo.todo_id)}
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+}
