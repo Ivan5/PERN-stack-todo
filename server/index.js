14aea100e17373c8f83015b56a7ec9680b9b4614
Ivan5 <converse_ivan@hotmail.com>
2020-03-24 11:14:49 -0600

    Setup server and connect to DB Postgres

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const pool = require("./db");
+
+///middlewares
+app.use(cors());
+
+app.listen(5000, () => {
+  console.log("Server is starting on port 5000");
+});
