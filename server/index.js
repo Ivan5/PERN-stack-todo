206f814cd884d919bc648bbf9bd34257a34aa878
Ivan5 <converse_ivan@hotmail.com>
2020-03-24 11:27:44 -0600

    Make an insert todo function

--- server/index.js
+++ server/index.js
@@ -5,6 +5,31 @@ const pool = require("./db");
 
 ///middlewares
 app.use(cors());
+app.use(express.json());
+
+//Routes//
+
+//get all todos
+
+//get a todo
+
+//create a todo
+app.post("/todos", async (req, res) => {
+  try {
+    const { description } = req.body;
+    const newTodo = await pool.query(
+      "INSERT INTO todo (description) VALUES ($1) RETURNING *",
+      [description]
+    );
+
+    res.json(newTodo);
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+//update a todo
+
+//delete a todo
 
 app.listen(5000, () => {
   console.log("Server is starting on port 5000");
